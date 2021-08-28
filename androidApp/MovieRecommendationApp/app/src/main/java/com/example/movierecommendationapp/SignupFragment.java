4cf078f6fdbc1c358b203685ae419c611f4c03ff
sudhay23 <sudhay2001@gmail.com>
2021-08-28 15:57:45 +0530

    Made some empty fragments

---
+++ androidApp/MovieRecommendationApp/app/src/main/java/com/example/movierecommendationapp/SignupFragment.java
@@ -0,0 +1,28 @@
+package com.example.movierecommendationapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SignupFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_signup, container, false);
+
+
+
+        return view;
+    }
+}
---
+++ androidApp/MovieRecommendationApp/app/src/main/java/com/example/movierecommendationapp/UserLoginSignupActivity.java
@@ -0,0 +1,14 @@
+package com.example.movierecommendationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserLoginSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login_signup);
+    }
+}
