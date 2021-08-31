0306531b8e4b11cb7675822479bfcbafd9021b1c
Sandeep Rajakrishnan <sandur43@gmail.com>
2021-08-31 23:26:39 +0530

    profile page accesses shared preferences

--- androidApp/MovieRecommendationApp/app/src/main/java/com/example/movierecommendationapp/ProfileActivity.java
+++ androidApp/MovieRecommendationApp/app/src/main/java/com/example/movierecommendationapp/ProfileActivity.java
@@ -2,7 +2,9 @@ package com.example.movierecommendationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,10 +15,12 @@ public class ProfileActivity extends AppCompatActivity implements UpdateProfileD
     private TextView textViewEmail;
     private TextView textViewPassword;
     private TextView textViewAge;
+    private TextView textViewPhno;
+
     private Button btnUpdateProfile;
     private Button btnSurfMovies;
 
-
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +30,12 @@ public class ProfileActivity extends AppCompatActivity implements UpdateProfileD
         textViewEmail = (TextView) findViewById(R.id.userEmail);
         textViewPassword = (TextView) findViewById(R.id.userPassword);
         textViewAge = (TextView) findViewById(R.id.userAge);
+        textViewPhno = (TextView) findViewById(R.id.userPhone);
+
         btnUpdateProfile = (Button) findViewById(R.id.updateProfileBtn);
         btnSurfMovies = (Button) findViewById(R.id.surfMoviesBtn);
+
+        setProfileDetails();
 
         btnUpdateProfile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,16 +54,43 @@ public class ProfileActivity extends AppCompatActivity implements UpdateProfileD
 
     }
 
+    private void setProfileDetails() {
+        preferences = getApplicationContext().getSharedPreferences("Register", Context.MODE_PRIVATE);
+        String registeredName = preferences.getString("Name", "passwordNotMatching");
+        String registeredPhone = preferences.getString("Phno", "passwordNotMatching");
+        String registeredAge = preferences.getString("Age", "passwordNotMatching");
+
+        String registeredEmail = preferences.getString("Email", "emailNotRegistered");
+        String registeredPassword = preferences.getString("Password", "passwordNotMatching");
+
+        textViewName.setText(registeredName);
+        textViewEmail.setText(registeredEmail);
+        textViewPassword.setText(registeredPassword);
+        textViewAge.setText(registeredAge);
+        textViewPhno.setText(registeredPhone);
+    }
+
     public void openDialog() {
         UpdateProfileDialog dialog = new UpdateProfileDialog();
         dialog.show(getSupportFragmentManager(), "Update Profile");
     }
 
     @Override
-    public void applyTexts(String username,String email, String password, String age) {
-        textViewName.setText(username);
-        textViewEmail.setText(email);
+    public void applyTexts(String name,String phno, String password, String age) {
+       /* textViewName.setText(name);
+        textViewPhno.setText(phone);
         textViewPassword.setText(password);
         textViewAge.setText(age);
+        */
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("Register", Context.MODE_PRIVATE);
+
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("Name",name);
+        editor.putString("Password",password);
+        editor.putString("Age",age);
+        editor.putString("Phno",phno);
+        editor.commit();
+        setProfileDetails();
     }
 }
