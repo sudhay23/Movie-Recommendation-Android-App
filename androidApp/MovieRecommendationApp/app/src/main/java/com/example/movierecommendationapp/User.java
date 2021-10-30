061d04f5e9679d16d9e8f8675424f400f53b76c5
sudhay23 <sudhay2001@gmail.com>
2021-10-30 20:28:18 +0530

    DAO file made for user signup

---
+++ androidApp/MovieRecommendationApp/app/src/main/java/com/example/movierecommendationapp/User.java
@@ -0,0 +1,57 @@
+package com.example.movierecommendationapp;
+
+public class User {
+    private String name, phoneNo, email, password;
+    private int age;
+
+    public User() {
+    }
+
+    public User(String name, String phoneNo, String email, String password, int age) {
+        this.name = name;
+        this.phoneNo = phoneNo;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
