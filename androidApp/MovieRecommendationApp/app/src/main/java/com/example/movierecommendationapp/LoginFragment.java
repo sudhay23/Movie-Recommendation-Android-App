89331b5aa02165e143839e7cff3d3a09c14e518c
Rajeshvaran <rajeshvara1991@gmail.com>
2021-11-08 21:25:48 +0530

    background and theme added

--- androidApp/MovieRecommendationApp/app/src/main/java/com/example/movierecommendationapp/LoginFragment.java
+++ androidApp/MovieRecommendationApp/app/src/main/java/com/example/movierecommendationapp/LoginFragment.java
@@ -22,8 +22,6 @@ import com.example.movierecommendationapp.cardview.CardMovieDetails;
 import com.example.movierecommendationapp.cardview.CardViewActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
